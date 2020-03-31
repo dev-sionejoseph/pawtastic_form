9bd35906f5e1237eebcc495bba4f575dc87deee9
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-03-30 20:30:20 -0500

    adjusted border radius

--- pawtastic/src/components/Form.js
+++ pawtastic/src/components/Form.js
@@ -34,49 +34,45 @@ export default class Form extends Component {
         <div id='formWrap'>
             <h1>Yay, we love dogs! Give us <br /> the basics about your pup.</h1>
             <form onSubmit={this.handleSubmit}>
-              <label>
-                Name
-                <input type="text" name='name' value={this.state.name} onChange={this.handleChange}/>
-                
-              </label>
-              <label >
-               <img src='#' alt='Camera Icon'></img>
-               <div id='uploadWrap'>
-                  <button id= 'upload'>Upload a photo</button>
-                  <input id='fileInput' type="file" name='photo' value={this.state.photo} placeholder='Upload a photo'/>
-               </div>
-              </label>
-  
-              <br/>
-  
-              <label>  
-                Breed
-                <input type="text" name='breed' value={this.state.breed} onChange={this.handleChange}/>
-              </label>
-              <label>
-                Birthday
-                <input type="text" name='birthday' value={this.state.birthday} onChange={this.handleChange}/>
-              </label>
-  
-              <br />
-  
-              <label>
-                Gender <br />
-                <select class='twoOptions' size="2">
-                  <option value="Female">Female</option>
-                  <option value="Male">Male</option>
-                </select>
-              </label>
-              <label>
-                Spayed or Neutered <br />
-                <select class='twoOptions' size="2">
-                  <option value="Yes">Yes</option>
-                  <option value="No">No</option>
-                </select>
-              </label>
-  
-              <br />
-              
+              <div id='rowOne'>
+                <label>
+                  Name
+                  <input type="text" name='name' value={this.state.name} onChange={this.handleChange}/>  
+                </label>
+                <label >
+                <img src='#' alt='Camera Icon'></img>
+                <div id='uploadWrap'>
+                    <button id= 'upload'>Upload a photo</button>
+                    <input id='fileInput' type="file" name='photo' value={this.state.photo} placeholder='Upload a photo'/>
+                </div>
+                </label>
+              </div>
+              <div id='rowTwo'>
+                <label>  
+                  Breed
+                  <input type="text" name='breed' value={this.state.breed} onChange={this.handleChange}/>
+                </label>
+                <label>
+                  Birthday
+                  <input type="text" name='birthday' value={this.state.birthday} onChange={this.handleChange}/>
+                </label>
+              </div>
+              <div id='rowThree'>
+                <label>
+                  Gender <br />
+                  <select class='twoOptions' size="2">
+                    <option value="Female">Female</option>
+                    <option value="Male">Male</option>
+                  </select>
+                </label>
+                <label>
+                  Spayed or Neutered <br />
+                  <select class='twoOptions' size="2">
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option>
+                  </select>
+                </label>
+              </div>
               <label>
                 Weight <br />
                 <select class='fourOptions' size="4">
@@ -87,7 +83,8 @@ export default class Form extends Component {
                 </select>
               </label>
             </form>
-            {/* <NavButtons /> */}
+            {/* <NavButtons />  */}
+            {/* importing here was getting weird. */}
         </div>
       );
     }
